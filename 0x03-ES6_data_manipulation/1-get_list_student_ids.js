4957affb777861162277d86f0625f6c63a447fb4
rooneyeno <swift2passion@gmail.com>
2023-08-04 20:20:17 +0000

    js frontend

---
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -0,0 +1,11 @@
+export default function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+  return arr.map((student) => student.id);
+}export default function getListStudentIds(arr) {
+  if (!Array.isArray(arr)) {
+    return [];
+  }
+  return arr.map((student) => student.id);
+}
